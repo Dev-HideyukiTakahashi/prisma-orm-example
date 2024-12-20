fb96791d50f20dd613247bf2c2fbdbd6379cd808
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-19 22:49:33 -0300

    repository e crud

--- index.ts
+++ index.ts
@@ -1,41 +1,126 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, User, Vehicle } from "@prisma/client"
 
 const prisma = new PrismaClient();
 
-async function searchUser(userEmail: string) {
-  return await prisma.user.findFirst({
+async function createUser(user: Omit<User, "id">) {
+  console.log("Criando novo usuario . . .")
+  const created = await prisma.user.create({
+    data: user
+  });
+
+  console.log(`----------Usuário criado id: ${created.id}----------`);
+  return created;
+}
+
+async function createVehicle(vehicle: Omit<Vehicle, "id">) {
+  console.log("Criando novo veículo . . .")
+  const created = await prisma.vehicle.create({
+    data: vehicle
+  });
+
+  console.log(`----------Veículo criado id: ${created.id}----------`);
+}
+
+async function getUsers() {
+  console.log("Buscando todos os usuários")
+  const users = await prisma.user.findMany();
+  users.forEach(user => {
+    console.log(user);
+  })
+  return users;
+}
+
+async function removeVehicles() {
+  const deleted = await prisma.vehicle.deleteMany({
     where: {
-      email: userEmail
+      id: {
+        gt: 0,
+      }
+    }
+  });
+  console.log("Veículos removidos...")
+  console.log(deleted);
+}
+
+async function removeUsers(users: Array<User>) {
+  users.forEach(async user => {
+    console.log("Deleting user with id: " + user.id)
+    await prisma.user.delete({
+      where: {
+        id: user.id
+      }
+    })
+  })
+}
+
+async function getUsersWithVehicle() {
+  // nessa busca é realizada um leftjoin, ou seja responde mesmo que usuario não tem vehicle
+  console.log("Buscando usuários com veículos");
+  return await prisma.user.findMany({
+    include: {
+      fleet: true
     }
   })
 }
 
-async function readUsers() {
-  return await prisma.user.findMany();
+async function getOnlyUsersWithVehicle() {
+  console.log("Buscando apenas usuários que possui veículos");
+  return await prisma.user.findMany({
+    include: {
+      fleet: true
+    },
+    where: {
+      fleet: {
+        some: {
+          NOT: {
+            id: 0
+          }
+        }
+      }
+    }
+  })
+
 }
 
-async function createUser() {
-  console.log('creating new user!')
-  const user = {
-    name: "Maria",
-    email: "maria@gmail.com"
-  };
-
-  return await prisma.user.create({ data: user });
+class UserRepository {
+  public async findByFirstNameAndLastName(firstName: string, lastName: string) {
+    return await prisma.user.findMany({
+      where: {
+        firstName, lastName
+      }
+    })
+  }
 }
 
 async function main() {
-  const usrs = await readUsers();
-  console.log(usrs)
+  const user = await createUser({ firstName: "Timber", lastName: "Saw" });
+  await createUser({ firstName: "Joao", lastName: "Silva" });
+  const usrs = await getUsers();
 
-  const usr = await searchUser("maria@gmail.com");
-  if (usr == null) {
-    console.log("creating user . . . ")
-    await createUser();
-  }
+  const vehicle = await createVehicle({ description: "Hyundai Tucson", year: 2022, userId: user.id });
+  const usrs2 = await getUsers();
 
-  const usrs2 = await readUsers();
-  console.log(usrs2)
+  const usersWithVehicle = await getUsersWithVehicle();
+  console.log(usersWithVehicle);
+
+  const onlyUsersWithVehicle = await getOnlyUsersWithVehicle()
+  console.log(onlyUsersWithVehicle)
+
+  const repository = new UserRepository()
+  const u = await repository.findByFirstNameAndLastName("Joao", "Silva");
+  console.log("----------Repository----------")
+  console.log(u);
+
+  await removeVehicles();
+  await removeUsers(usrs);
 }
 
-main();
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    await prisma.$disconnect();
+    console.log(e);
+    process.exit(1);
+  })
